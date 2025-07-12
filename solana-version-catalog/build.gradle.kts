39a978a230847e7361ea1e53484e5c272ef5c168
jpe7s <jpe7s.salt188@passfwd.com>
2025-07-12 15:47:49 -0500

    Bump sava.

--- solana-version-catalog/build.gradle.kts
+++ solana-version-catalog/build.gradle.kts
@@ -35,7 +35,7 @@ val savaJsonIterator = "21.0.11"
 val bouncyCastle = "1.81"
 
 // https://central.sonatype.com/search?namespace=software.sava
-val sava = "24.19.8"
+val sava = "24.19.9"
 val savaWeb2 = "24.0.0"
 val savaPrograms = "24.20.3"
 val savaSrcGen = "24.0.2"
