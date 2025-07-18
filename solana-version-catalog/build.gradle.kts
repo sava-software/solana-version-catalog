4786c81387a45e3767ae7608e8b6a8e2bdb1db6b
jpe7s <jpe7s.salt188@passfwd.com>
2025-07-18 14:28:06 -0500

    Bump src gen

--- solana-version-catalog/build.gradle.kts
+++ solana-version-catalog/build.gradle.kts
@@ -38,7 +38,7 @@ val bouncyCastle = "1.81"
 val sava = "24.19.9"
 val savaWeb2 = "24.0.1"
 val savaPrograms = "24.20.3"
-val savaSrcGen = "24.0.4"
+val savaSrcGen = "24.0.5"
 val savaAnchorPrograms = "24.0.17"
 
 val glamIxProxy = "24.0.0"
