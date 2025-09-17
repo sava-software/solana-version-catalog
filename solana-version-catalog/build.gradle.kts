727ae628cf92f81f782d0f54b0954fee5810e75e
jpe7s <jpe7s.salt188@passfwd.com>
2025-09-17 11:06:51 -0500

    update gradle. bump ravina.

--- solana-version-catalog/build.gradle.kts
+++ solana-version-catalog/build.gradle.kts
@@ -47,7 +47,7 @@ val glamIxProxy = "24.0.0"
 val jetty = "12.1.1"
 // https://central.sonatype.com/artifact/com.google.cloud/google-cloud-kms
 val googleCloudKms = "2.76.0"
-val savaRavina = "24.0.2"
+val savaRavina = "25.0.0"
 
 // https://mvnrepository.com/artifact/io.grpc
 val grpc = "1.75.0"
