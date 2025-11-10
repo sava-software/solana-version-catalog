f73ca89fab4fbbe3193b3552b679134be4f2f0c5
jpe7s <jpe7s.salt188@passfwd.com>
2025-11-10 16:16:58 -0600

    bump glam sdk.

--- solana-version-catalog/build.gradle.kts
+++ solana-version-catalog/build.gradle.kts
@@ -50,7 +50,7 @@ val savaIDLClientsOracles = savaIDLClients
 val savaIDLClientsSPL = savaIDLClients
 
 val glamIxProxy = "25.0.2"
-val glamSDK = "25.0.4"
+val glamSDK = "25.0.5s"
 
 // https://central.sonatype.com/artifact/org.eclipse.jetty/jetty-server
 val jetty = "12.1.3"
