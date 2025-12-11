d76285cdd8725b3e4aaf01aaf8434a7e24f35b82
jpe7s <jpe7s.salt188@passfwd.com>
2025-12-11 13:22:16 -0600

    Update IDL Clients. Optional use of GHPR.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,23 @@
 pluginManagement {
   repositories {
     gradlePluginPortal()
-    maven {
-      name = "savaGithubPackages"
-      url = uri("https://maven.pkg.github.com/sava-software/sava-build")
-      credentials(PasswordCredentials::class)
+    mavenCentral()
+    if (
+      providers.gradleProperty("savaGithubPackagesUsername").isPresent &&
+      providers.gradleProperty("savaGithubPackagesPassword").isPresent
+    ) {
+      maven {
+        name = "savaGithubPackages"
+        url = uri("https://maven.pkg.github.com/sava-software/sava-build")
+        credentials(PasswordCredentials::class)
+      }
     }
-  }
 //  includeBuild("../sava-build")
+  }
 }
 
 plugins {
-  id("software.sava.build") version "0.3.0"
+  id("software.sava.build") version "21.3.2"
 }
 
 rootProject.name = "solana-version-catalog"
