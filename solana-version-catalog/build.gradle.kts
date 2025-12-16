20349927b612d6538fca9c350c0776c7ce352ade
jpe7s <jpe7s.salt188@passfwd.com>
2025-12-16 08:41:31 -0600

    Update idl clients.

--- solana-version-catalog/build.gradle.kts
+++ solana-version-catalog/build.gradle.kts
@@ -40,7 +40,7 @@ val savaWeb2 = "25.1.0"
 val savaPrograms = "25.0.0"
 val savaAnchorPrograms = "25.1.4"
 
-val savaIDLClients = "25.7.1"
+val savaIDLClients = "25.8.0"
 val savaIDLClientsCore = savaIDLClients
 val savaIDLClientsCCTP = savaIDLClients
 val savaIDLClientsDrift = savaIDLClients
