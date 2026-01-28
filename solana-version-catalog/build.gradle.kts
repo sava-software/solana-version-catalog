83814639d51f95702da15fd25e5b50f5596e1a30
jpe7s <jpe7s.salt188@passfwd.com>
2026-01-28 08:01:07 -0600

    Update sava.

--- solana-version-catalog/build.gradle.kts
+++ solana-version-catalog/build.gradle.kts
@@ -35,7 +35,7 @@ val savaJsonIterator = "21.0.11"
 val bouncyCastle = "1.81"
 
 // https://central.sonatype.com/search?namespace=software.sava
-val sava = "25.2.0"
+val sava = "25.3.0"
 val savaWeb2 = "25.1.0"
 val savaPrograms = "25.0.0"
 val savaAnchorPrograms = "25.1.4"
