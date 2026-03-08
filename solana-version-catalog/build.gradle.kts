56cd5b3d2f8bd00c34a505e75e461dc4bb05c11c
jpe7s <jpe7s.salt188@passfwd.com>
2026-03-08 09:15:30 -0500

    Bump glam.

--- solana-version-catalog/build.gradle.kts
+++ solana-version-catalog/build.gradle.kts
@@ -53,7 +53,7 @@ val savaIDLClientsOracles = savaIDLClients
 val savaIDLClientsSPL = savaIDLClients
 
 val glamIxProxy = "25.0.2"
-val glamSDK = "25.5.1"
+val glamSDK = "25.5.3"
 
 // https://central.sonatype.com/artifact/org.postgresql/postgresql
 val postgresql = "42.7.10"
