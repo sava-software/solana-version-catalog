4ff166cc57484b7ff3e7a45a3329093e471c7300
jpe7s <jpe7s.salt188@passfwd.com>
2026-06-01 15:24:15 -0500

    fix(deps): bump glamSDK to 25.12.0
    - Update `glamSDK` version to include latest fixes and enhancements.

--- solana-version-catalog/build.gradle.kts
+++ solana-version-catalog/build.gradle.kts
@@ -57,7 +57,7 @@ val savaIDLClients = "25.12.11"
 //val savaIDLClientsSPL = savaIDLClients
 
 val glamIxProxy = "25.0.4"
-val glamSDK = "25.11.0"
+val glamSDK = "25.12.0"
 
 // https://central.sonatype.com/artifact/org.postgresql/postgresql
 val postgresql = "42.7.11" // https://github.com/pgjdbc/pgjdbc/releases/tag/REL42.7.11
