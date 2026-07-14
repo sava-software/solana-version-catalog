479509d0c2c4b2d363e3dbe451cdc944e5687d3e
jpe7s <jpe7s.salt188@passfwd.com>
2026-07-14 18:00:49 -0500

    fix(deps): bump savaJsonIterator to 25.1.2
    - Upgrade `savaJsonIterator` to version 25.1.2 for minor fixes and improvements.

--- solana-version-catalog/build.gradle.kts
+++ solana-version-catalog/build.gradle.kts
@@ -25,7 +25,7 @@ val junit = "6.1.2"
 // Compile & Implementation
 
 // https://github.com/sava-software/json-iterator
-val savaJsonIterator = "25.1.1"
+val savaJsonIterator = "25.1.2"
 
 // https://mvnrepository.com/artifact/org.bouncycastle/bcprov-jdk18on
 // https://www.bouncycastle.org/download/bouncy-castle-java/#latest
